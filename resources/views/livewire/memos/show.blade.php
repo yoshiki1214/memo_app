7246f3a82b4c517fd5194a3ad3f0787dc9662133
yoshiki1214 <gazerk43@gmail.com>
2025-08-31 11:37:46 +0900

    一覧機能まで完成

--- resources/views/livewire/memos/show.blade.php
+++ resources/views/livewire/memos/show.blade.php
@@ -9,6 +9,7 @@ state(['memo' => fn(Memo $memo) => $memo]);
 ?>
 
 <div>
+    <a href="{{ route('memos.index') }}">戻る</a>
     <h1>{{ $memo->title }}</h1>
     <p>{!! nl2br(e($memo->body)) !!}</p>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//一覧ページ
+Volt::route('/memos', 'memos.index')->name('memos.index');
 //詳細ページ
 Volt::route('/memos/{memo}', 'memos.show')->name('memos.show');
